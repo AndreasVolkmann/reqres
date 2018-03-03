739a12e84e806f1b172e81be845c92067b8d6fef
AndreasVolkmann <avo@apurebase.com>
2018-03-03 10:27:55 +0100

    inser into db

--- RequestReader.go
+++ RequestReader.go
@@ -6,6 +6,8 @@ import (
 	"bufio"
 	"strings"
 	"strconv"
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func check(e error) {
@@ -21,29 +23,46 @@ type request struct {
 	date string
 }
 
-func Map(vs []string, f func(string) request) []request {
-	vsm := make([]request, len(vs))
-	for i, v := range vs {
-		vsm[i] = f(v)
-	}
-	return vsm
-}
+
 
 func main() {
 	path := "D:\\Dev\\go\\src\\github.com\\AndreasVolkmann\\reqres\\request.csv"
 	lines, err := buffered(path)
 	check(err)
 	fmt.Println("Requests:")
-	requests := Map(lines, func(line string) request {
-		split := strings.Split(line, ",")
-		id, err := strconv.Atoi(split[0])
-		check(err)
-		return request{id, split[1], split[2], split[3]}
-	})
+	requests := transformLines(lines)
 	for _, v := range requests {
 		fmt.Println(v)
 	}
 
+	insertRequests(requests)
+}
+
+func insertRequests(requests []request) {
+	db, err := sql.Open("mysql", "test:test@/reqres")
+	check(err)
+	defer db.Close()
+
+	stmtIns, err := db.Prepare("INSERT INTO reqres.requests VALUES (?, ?, ?, ?)")
+	check(err)
+	defer stmtIns.Close()
+	for _, req := range requests {
+		_, err = stmtIns.Exec(req.id, req.user, req.note, req.date)
+		check(err)
+	}
+}
+
+func transformLines(lines []string) []request {
+	return Map(lines, func(line string) request {
+		return transformLineToRequest(line)
+	})
+}
+
+func transformLineToRequest(line string) request {
+	split := strings.Split(line, ",")
+	id, err := strconv.Atoi(split[0])
+	check(err)
+	return request{id, split[1], split[2], split[3]}
 }
 
 func buffered(path string) ([]string, error) {
@@ -58,4 +77,12 @@ func buffered(path string) ([]string, error) {
 	}
 	lines = append(lines[:0], lines[1:]...) // drop header
 	return lines, sc.Err()
+}
+
+func Map(vs []string, f func(string) request) []request {
+	vsm := make([]request, len(vs))
+	for i, v := range vs {
+		vsm[i] = f(v)
+	}
+	return vsm
 }
